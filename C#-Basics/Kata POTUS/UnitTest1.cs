49e8d3b8cfaa6069c423e49fbf0e773e6bd77b47
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-03-06 13:38:25 +0100

    Finish second kata

---
+++ C#-Basics/Kata POTUS/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Kata_POTUS
+{
+    public class Tests
+    {
+        [Test]
+        public void Ex1()
+        {
+            char[][] house =
+            {
+            "  o                o        #######".ToCharArray(),
+            "###############             #     #".ToCharArray(),
+            "#             #        o    #     #".ToCharArray(),
+            "#  X          ###############     #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "###################################".ToCharArray()
+        };
+            Assert.AreEqual(true, Dinglemouse.AllAlone(house));
+        }
+
+        [Test]
+        public void Ex2()
+        {
+            char[][] house =
+            {
+            "#################             ".ToCharArray(),
+            "#     o         #   o         ".ToCharArray(),
+            "#          ######        o    ".ToCharArray(),
+            "####       #                  ".ToCharArray(),
+            "   #       ###################".ToCharArray(),
+            "   #                         #".ToCharArray(),
+            "   #                  X      #".ToCharArray(),
+            "   ###########################".ToCharArray()
+        };
+            Assert.AreEqual(false, Dinglemouse.AllAlone(house));
+        }
+    }
+}
---
+++ C#-Basics/TextFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
